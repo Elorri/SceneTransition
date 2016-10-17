9767f8d0f1e708efbaffe4870a7588d2aee8cb36
Elorri <etchemendy.elorri@gmail.com>
2016-10-17 08:00:33 +0200

    Save

--- app/src/main/java/com/example/android/scenetransition/MainActivity.java
+++ app/src/main/java/com/example/android/scenetransition/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.android.scenetransition;
 
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.transition.AutoTransition;
@@ -7,8 +8,11 @@ import android.transition.Scene;
 import android.transition.Transition;
 import android.transition.TransitionManager;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.FrameLayout;
+import android.widget.PopupWindow;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
     private View largeMenu;
 
     boolean displayInNewWindow;
+    private FrameLayout container;
 
 
     @Override
@@ -28,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
 
     private void initToggle() {
 
-        final FrameLayout container = (FrameLayout) findViewById(R.id.container);
+        container = new FrameLayout(getApplicationContext());
 
         smallMenu = getLayoutInflater().inflate(R.layout.closed, container, false);
         largeMenu = getLayoutInflater().inflate(R.layout.opened, container, false);
@@ -38,24 +43,44 @@ public class MainActivity extends AppCompatActivity {
         clickMe.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                View finalView = null;
-                Scene finalScene = null;
-                final Transition t = new AutoTransition();
-                t.setDuration(500);
+                 View finalView = null;
                 if (displayInNewWindow()) {
                     finalView = getContentView();
-                    finalScene = new Scene(container, finalView);
-                    finalScene.enter(); //Calling this will make final scene text appearing on
-                    // before container has finished resizing.
-                    TransitionManager.go(finalScene, t);//This will make the container.addView no
-                    // need to do it ourselves
+                    PopupWindow pw=createWindow();
+                    setWindowContentView(container, finalView);
+                    show(pw, v);
                 } else {
                     finalView = getContentView();
-                    finalScene = new Scene(container, finalView);
-                    TransitionManager.go(finalScene, t);
+                    setWindowContentView(container, finalView);
                 }
             }
         });
+    }
+
+    private void show(PopupWindow popupWindow, View anchor) {
+        Rect anchorLocation = DialogUtils.locateView(anchor);
+        popupWindow.showAtLocation(anchor, Gravity.NO_GRAVITY, anchorLocation.right, anchorLocation.top);
+    }
+
+    private PopupWindow createWindow() {
+        PopupWindow popupWindow = new PopupWindow();
+        popupWindow.setWidth(WindowManager.LayoutParams.WRAP_CONTENT);
+        popupWindow.setHeight(WindowManager.LayoutParams.WRAP_CONTENT);
+//        popupWindow.setOutsideTouchable(true);
+//        popupWindow.setFocusable(true);
+//        popupWindow.setBackgroundDrawable(new BitmapDrawable());
+        popupWindow.setContentView(container);
+        return popupWindow;
+    }
+
+    private void setWindowContentView(FrameLayout container, View finalView) {
+        Scene finalScene = new Scene(container, finalView);
+        finalScene.enter(); //Calling this will make final scene text appearing on
+        // before container has finished resizing.
+        final Transition t = new AutoTransition();
+        t.setDuration(500);
+        TransitionManager.go(finalScene, t);//This will make the container.addView no
+        // need to do it ourselves
     }
 
     private boolean displayInNewWindow() {
