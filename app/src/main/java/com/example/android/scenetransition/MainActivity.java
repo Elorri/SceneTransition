95c3db517de8f76ab6fa97bf7ac49d26e75e9251
Elorri <elorri.etchemendy@myscript.com>
2016-10-21 18:14:49 +0200

    toggle in main layout

--- app/src/main/java/com/example/android/scenetransition/MainActivity.java
+++ app/src/main/java/com/example/android/scenetransition/MainActivity.java
@@ -7,50 +7,68 @@ import android.transition.Scene;
 import android.transition.Transition;
 import android.transition.TransitionManager;
 import android.view.View;
-import android.widget.RelativeLayout;
+import android.view.ViewGroup;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    private View viewA;
+    private View viewB;
+
+    private View oldView;
+    private View currentView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         initToggle();
-     }
+    }
 
     private void initToggle() {
+        final ViewGroup container = (ViewGroup) findViewById(R.id.container);
+        viewA = getLayoutInflater().inflate(R.layout.menu_a, container, false);
+        viewB = getLayoutInflater().inflate(R.layout.menu_b, container, false);
+        currentView = viewA;
 
-        final RelativeLayout sceneBase = (RelativeLayout) findViewById(R.id.toggle);
+        final Scene sceneStart = new Scene(container, viewA);
+        sceneStart.enter();
 
-        final View scene2closed =  getLayoutInflater().inflate(R.layout.closed, sceneBase, false);
-        final View scene2opened =  getLayoutInflater().inflate(R.layout.opened, sceneBase, false);
+        findViewById(R.id.toggle).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                toggle(container, getFinalScene());
+            }
+        });
+    }
 
-        final Scene sceneClose = new Scene(sceneBase, scene2closed);
-        sceneClose.enter();
 
-        final Scene sceneOpen = new Scene(sceneBase, scene2opened);
+    private View getFinalScene() {
+        if (currentView.equals(viewA)) {
+            oldView = currentView;
+            currentView = viewB;
+            return viewB;
+        } else {
+            oldView = currentView;
+            currentView = viewA;
+            return viewA;
+        }
+    }
 
+    private void toggle(View container, View finalView) {
         final Transition t = new AutoTransition();
-        t.setDuration(500);
+        t.setDuration(3000);
 
-//        final ViewGroup scene2closed = (ViewGroup)mInflater.inflate(R.layout.include_map_select_closed, sceneBase, false);
-//        final Scene sceneClose = new Scene(sceneBase, scene2closed);
-//        sceneClose.enter();
+//        if (oldView!=null){
+//            Scene oldScene = new Scene((ViewGroup) container, oldView);
+//            oldScene.exit();
+//            TransitionManager.go(oldScene, t);
+//        }
 
-        scene2closed.findViewById(R.id.toggle_scene).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                TransitionManager.go(sceneOpen, t);
-            }
-        });
+        Scene finalScene = new Scene((ViewGroup) container, finalView);
+        TransitionManager.go(finalScene, t);
 
-        scene2opened.findViewById(R.id.button1).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                TransitionManager.go(sceneClose, t);
-            }
-        });
     }
 }
